4f1d7698f23e118dd31061986a198d4862ffdde1
Jmattgiroux <git config --global user.email jmattgiroux@gmail.comgit config --global user.email>
2020-12-11 12:04:56 -0600

    navigation for app implemented

--- app/src/main/java/com/example/rockpaperscissor/Game.java
+++ app/src/main/java/com/example/rockpaperscissor/Game.java
@@ -10,12 +10,57 @@ import android.widget.Button;
 public class Game extends AppCompatActivity {
 
 
+    Button rock, paper, scissors, quit, back;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        rock = (Button) findViewById(R.id.rockButton);
+        paper = (Button) findViewById(R.id.paperButton);
+        scissors = (Button) findViewById(R.id.scissorsButton);
+        quit = (Button) findViewById(R.id.quitButton);
+        back = (Button) findViewById(R.id.gameBackButton);
+
+        rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        paper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        scissors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Game.this, Records.class);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Game.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/rockpaperscissor/Records.java
+++ app/src/main/java/com/example/rockpaperscissor/Records.java
@@ -2,13 +2,28 @@ package com.example.rockpaperscissor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Records extends AppCompatActivity {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_records);
+
+        back = (Button) findViewById(R.id.recordsBackButton);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Records.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
