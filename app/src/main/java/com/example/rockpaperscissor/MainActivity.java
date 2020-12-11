6aaff83e3b2bfa7911d0a6cd7369a073b3d0527b
Jmattgiroux <git config --global user.email jmattgiroux@gmail.comgit config --global user.email>
2020-12-11 11:54:53 -0600

    MainActivity is implemented

--- app/src/main/java/com/example/rockpaperscissor/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissor/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.rockpaperscissor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button playButton, recordsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        playButton = (Button) findViewById(R.id.playGameButton);
+        recordsButton = (Button) findViewById(R.id.topScoresButton);
+
+
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Game.class);
+                startActivity(intent);
+            }
+        });
+
+        recordsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Records.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
